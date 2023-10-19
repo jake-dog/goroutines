18d54fa3dc638f6e0acbab966b866fee8bc6e887
jake-dog <jake-dog@users.noreply.github.com>
2023-10-18 21:00:34 -0400

    cleaning up code and docs

--- queuedrunner.go
+++ queuedrunner.go
@@ -17,13 +17,6 @@ var zeroTime = time.Time{}
 // ErrRunnerTimedout means a timeout occured waiting for result of "fn"
 var ErrRunnerTimedout = errors.New("runner timed out")
 
-// RunnerFn is a generic function that can be used with QueuedRunner.
-// If error is non-nil then a result will not be cached.  To cache an error,
-// the returned error must be nil and the value of result is an error.
-// If both error and result are nil, the result is not cached.
-// The result may be concurrently accessed and must not be modified.
-//type RunnerFn func() (result any, err error)
-
 // QueuedRunner serializes access to a function, allowing multiple goroutines
 // to queue returning identical result to all waiters.  The caller must
 // not retain or modify result unless safe to do so given function "fn".
@@ -51,7 +44,10 @@ func NewQueuedRunner[T any](fn func() (T, error)) *QueuedRunner[T] {
 // A cached result is returned if available and the result is not older than
 // ttl+grace.  If the result is older than ttl but younger than grace+ttl,
 // a cached result is returned and a call to "fn" is made concurrently,
-// refreshing the cached result.
+// refreshing the cached result.  When run if error is non-nil then a result
+// will not be cached.  To cache an error, the returned error must be nil and
+// the value of result an error.
+// The result may be concurrently accessed and must not be modified.
 func NewCachedQueuedRunner[T any](fn func() (T, error), ttl time.Duration, grace time.Duration) *QueuedRunner[T] {
 	return &QueuedRunner[T]{
 		mu:    new(sync.Mutex),
@@ -61,69 +57,46 @@ func NewCachedQueuedRunner[T any](fn func() (T, error), ttl time.Duration, grace
 	}
 }
 
-
-/*func (qr *QueuedRunner) RunTimeout(timeout time.Duration) (any, error) {
-	return qr.run(timeout, false)
-}
-
-// RunCachedTimeout mimics RunTimeout but returns a cached result if available
-// Can be called from mulitple goroutines ensuring only one invocation of "fn"
-// is active at a time.
-func (qr *QueuedRunner) RunCachedTimeout(timeout time.Duration) (any, error) {
-	return qr.run(timeout, true)
-}
-
-// Run or queue for the next result of "fn"
-// Can be called from mulitple goroutines ensuring only one invocation of "fn"
-// is active at a time.
-func (qr *QueuedRunner) Run() (any, error) {
-	return qr.run(-1, false)
-}
-
-// RunCached returns the next result of "fn", or cached value if available
-// Can be called from mulitple goroutines ensuring only one invocation of "fn"
-// is active at a time.
-func (qr *QueuedRunner) RunCached() (any, error) {
-	return qr.run(-1, true)
-}*/
-
 type UncachedQueuedRunner[T any] struct{
 	qr *QueuedRunner[T]
 }
 
+// TryRun but do not return cached values
 func (u UncachedQueuedRunner[T]) TryRun() (T, error) {
 	return u.qr.run(context.Background(), 0, true)
+}
+
+// Run but do not return cached values
+func (u UncachedQueuedRunner[T]) Run() (T, error) {
+	return u.qr.run(context.Background(), -1, true)
+}
+
+// RunWithContext but do not return cached values
+func (u UncachedQueuedRunner[T]) RunWithContext(ctx context.Context) (T, error) {
+	return u.qr.run(ctx, -1, true)
+}
+
+// RunWithTimeout but do not return cached values
+func (u UncachedQueuedRunner[T]) RunWithTimeout(timeout time.Duration) (T, error) {
+	return u.qr.run(context.Background(), timeout, true)
+}
+
+// TryRun returns immediately with a result if available or ErrRunnerTimedout.
+// Identical to TryWithTimeout(0)
+func (qr *QueuedRunner[T]) TryRun() (T, error) {
+	return qr.run(context.Background(), 0, false)
 }
 
 // Run or queue for the next result of "fn"
 // Can be called from mulitple goroutines ensuring only one invocation of "fn"
 // is active at a time.
-func (u UncachedQueuedRunner[T]) Run() (T, error) {
-	return u.qr.run(context.Background(), -1, true)
-}
-
-func (u UncachedQueuedRunner[T]) RunWithContext(ctx context.Context) (T, error) {
-	return u.qr.run(ctx, -1, true)
-}
-
-// RunTimeout runs or queues until timeout for the next result of "fn". If
-// timeout is zero, return immediately with response or ErrRunnerTimeout when
-// no result is available.  If timeout is positive return ErrRunnerTimeout
-// when timeout occurs.  Wait for result when timeout is negative.
-// Can be called from mulitple goroutines ensuring only one invocation of "fn"
-// is active at a time.
-func (u UncachedQueuedRunner[T]) RunWithTimeout(timeout time.Duration) (T, error) {
-	return u.qr.run(context.Background(), timeout, true)
-}
-
-func (qr *QueuedRunner[T]) TryRun() (T, error) {
-	return qr.run(context.Background(), 0, false)
-}
-
 func (qr *QueuedRunner[T]) Run() (T, error) {
 	return qr.run(context.Background(), -1, false)
 }
 
+// RunWithContextruns or queues for the next result of "fn".
+// Can be called from mulitple goroutines ensuring only one invocation of "fn"
+// is active at a time.
 func (qr *QueuedRunner[T]) RunWithContext(ctx context.Context) (T, error) {
 	return qr.run(ctx, -1, false)
 }
@@ -138,6 +111,7 @@ func (qr *QueuedRunner[T]) RunWithTimeout(timeout time.Duration) (T, error) {
 	return qr.run(context.Background(), timeout, false)
 }
 
+// UncachedQueuedRunner returns the same QueuedRunner with cache bypass enabled
 func (qr *QueuedRunner[T]) NoCache() UncachedQueuedRunner[T] {
 	return UncachedQueuedRunner[T]{qr}
 }
@@ -146,7 +120,7 @@ func (qr *QueuedRunner[T]) run(ctx context.Context, timeout time.Duration, noCac
 	var gen int
 	qr.mu.Lock()
 
-	if !noCache && qr.ttl > 0  && time.Since(qr.added) <= qr.ttl {
+	if !noCache && qr.ttl > 0 && time.Since(qr.added) <= qr.ttl {
 		defer qr.mu.Unlock()
 		return qr.result, nil
 	}
